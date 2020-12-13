7e95a55ade429c509f0f3e0f9e431efa0d59379e
Bhavesh Joshi <34483890+gokulbhaveshjoshi@users.noreply.github.com>
2020-12-13 10:42:11 +0530

    Remove unwanted files
    Remove unwanted files and add confussion matrix and heat map in file

--- fake_news_detection.py
+++ fake_news_detection.py
@@ -7,15 +7,18 @@ Fake News Detection System
 
 
 # Import libraries
-
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import itertools as iter
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pickle
+import seaborn as sn
+
 
 # cleaning file text and lebal
 news = pd.read_csv("datasets/fake_or_real_news.csv")
@@ -36,9 +39,68 @@ pipeline.fit(X_train, y_train)
 pred = pipeline.predict(X_test)
 
 #Checking the performance of our model
+
+print("Classification Report")
 print(classification_report(y_test, pred))
-print(confusion_matrix(y_test, pred))
+
+
+print("Confusion Matrix Value")
+array = confusion_matrix(y_test, pred)
+print(array)
+# Heat Map
+df_cm = pd.DataFrame(array, index = [i for i in "AB"],
+                  columns = [i for i in "AB"])
+plt.figure(figsize = (10,7))
+sn.heatmap(df_cm, annot=True)
 
 #Serialising the file
 with open('model.pickle', 'wb') as handle:
     pickle.dump(pipeline, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    
+    
+    
+    
+#Evaluation of Model - Confusion Matrix Plot
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in iter.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.tight_layout()
+
+
+# Compute confusion matrix
+cnf_matrix = confusion_matrix(y_test, pred)
+np.set_printoptions(precision=2)
+
+# Plot non-normalized confusion matrix
+plt.figure(figsize = (10,7))
+plot_confusion_matrix(cnf_matrix, classes=['Fake','Real'],
+                      title='Confusion matrix, without normalization')
